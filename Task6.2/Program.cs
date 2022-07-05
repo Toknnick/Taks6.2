4892225efb3a57ddc0cd2353159daa29db4f4586
Toknnick <106018075+Toknnick@users.noreply.github.com>
2022-07-05 12:29:01 +0300

    Update Program.cs

--- Task6.2/Program.cs
+++ Task6.2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Task6._2
 {
@@ -9,26 +9,30 @@ namespace Task6._2
             int positionX = 10;
             int positionY = 5;
             Player player = new Player(positionX, positionY);
-            player.Draw('%');
+            Renderer renderer = new Renderer();
+            renderer.Draw(player.PositionX, player.PositionY, '#');
             Console.ReadKey();
+        }
+    }
+    
+    class Renderer
+    {
+        public void Draw(int positionX, int positionY, char symbol)
+        {
+            Console.SetCursorPosition(positionX, positionY);
+            Console.Write(symbol);
         }
     }
 
     class Player
     {
-        public int X { get; private set; }
-        public int Y { get; private set; }
+        public int PositionX { get; private set; }
+        public int PositionY { get; private set; }
 
         public Player(int x, int y)
         {
-            X = x;
-            Y = y;
-        }
-
-        public void Draw(char symbol)
-        {
-            Console.SetCursorPosition(X, Y);
-            Console.Write(symbol);
+            PositionX = x;
+            PositionY = y;
         }
     }
 }
